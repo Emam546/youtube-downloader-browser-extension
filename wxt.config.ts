5152bb29f6e28f5a4c2a6054f8edd957a903685f
Emam546 <emam54637@gmail.com>
2026-05-30 00:58:47 +0300

    fix release and do some adjustments

--- wxt.config.ts
+++ wxt.config.ts
@@ -1,10 +1,16 @@
 import { defineConfig } from "wxt";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
+import packageJson from "./package.json";
 // See https://wxt.dev/api/config.html
 export default defineConfig({
   modules: ["@wxt-dev/module-react"],
+  zip: {
+    artifactTemplate: "youtube-downloader-v{{version}}-{{browser}}.zip",
+  },
   manifest: {
+    version: packageJson.version,
+
     name: "Youtube Downloader",
     description: "Send the current video/page URL to the downloader app",
     icons: {
